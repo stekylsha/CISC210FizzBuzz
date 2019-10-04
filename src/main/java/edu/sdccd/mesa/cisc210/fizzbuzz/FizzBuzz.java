bff516abcd77fce02493ac630346cede3c710649
Steven K. Sharp <steven.k.sharp@gmail.com>
2019-10-03 18:06:30 -0700

    initial commit

---
+++ src/main/java/edu/sdccd/mesa/cisc210/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,27 @@
+package edu.sdccd.mesa.cisc210.fizzbuzz;
+
+public class FizzBuzz {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        for (int i = 1 ; i < 42; i++) {
+            System.out.println(i + ": " + fb.doFb(i));
+        }
+    }
+    
+    public String doFb(int i) {
+        String fb = "";
+        if ((i % 3) == 0) {
+            fb += "fizz";
+        }
+        if ((i % 5) == 0) {
+            fb += "buzz";
+        }
+        if (fb.isEmpty()) {
+            fb = ".";
+        }
+        return fb;
+    }
+}
