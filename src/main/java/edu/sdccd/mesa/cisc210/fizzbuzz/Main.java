6700067a58eedf9284ce0713b3af3fd392818c20
stekylsha <steven.k.sharp@gmail.com>
2019-11-12 23:37:49 -0800

    Chain of responsibility version of fizzbuzz.

--- src/main/java/edu/sdccd/mesa/cisc210/fizzbuzz/Main.java
+++ src/main/java/edu/sdccd/mesa/cisc210/fizzbuzz/Main.java
@@ -1,26 +1,11 @@
 package edu.sdccd.mesa.cisc210.fizzbuzz;
 
+import edu.sdccd.mesa.cisc210.fizzbuzz.controller.FBController;
+
 public class Main {
 
-    private FizzBuzz fb;
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Main fbc = new Main();
-        fbc.control();
-    }
-
-    private Main() {
-        fb = new FizzBuzz();
-    }
-
-    private void control() {
-        System.out.println();
-        for (int i = 1 ; i < 42; i++) {
-            fb.doFb(i);
-        }
-        System.out.println();
+    public static void main(String... args) {
+        Controller controller = new FBController();
+        controller.control();
     }
 }
